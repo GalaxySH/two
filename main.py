d7e65be6f2151cbb1d801efc24520f2a5911d26d
Rooskie <polyversialmind@gmail.com>
2021-07-23 20:58:42 -0500

    finish whole game with room for more

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ from cocos.scene import Scene
 from cocos.text import Label
 from pyglet.window import key
 from pyglet.image import load as iload, ImageGrid, Animation
-from pyglet.media import load as mload
+from pyglet.media import load as mload, Player
 from random import random
+
+shoot_sfx = mload('sfx/shoot.wav', streaming=False)
+kill_sfx = mload("sfx/invaderkilled.wav", streaming=False)
+die_sfx = mload('sfx/explosion.wav', streaming=False)
+bg_music = mload('sfx/level1.ogg', streaming=False)
+
 
 def load_animation(image, rows=2, cols=1, fr=0.5):
     seq = ImageGrid(iload(image), rows, cols)
@@ -24,7 +30,7 @@ SPECIES = {
 
 class Actor(Sprite):
     def __init__(self, image, x, y):
-        super(Actor, self).__init__(image)
+        super().__init__(image)
 
         pos = Vector2(x, y)
         self.position = pos
@@ -44,7 +50,7 @@ class Actor(Sprite):
 
 class PlayerCannon(Actor):
     def __init__(self, x, y):
-        super(PlayerCannon, self).__init__("img/cannon.png", x, y)
+        super().__init__("img/cannon.png", x, y)
 
         self.speed = Vector2(200, 0)
 
@@ -68,9 +74,10 @@ class PlayerCannon(Actor):
         ):  # can move
             self.move(magnitude)
 
-        is_firing = keyboard[key.SPACE]
+        is_firing = keyboard[key.SPACE] or keyboard[key.UP]
         if PlayerShoot.ACTIVE_SHOOT is None and is_firing:
             self.parent.add(PlayerShoot(self.x, self.y + 50))
+            shoot_sfx.play()
 
         # if self.parent.collide_check(self):
         #     self.parent.respawn_player()
@@ -80,13 +87,13 @@ class PlayerShoot(Actor):
     ACTIVE_SHOOT = None
 
     def __init__(self, x, y):
-        super(PlayerShoot, self).__init__(load_animation('img/missile2.png', fr=1), x, y)
+        super().__init__(load_animation('img/missile2.png', fr=0.2), x, y)
 
         self.speed = Vector2(0, 400)
         PlayerShoot.ACTIVE_SHOOT = self
 
     def on_exit(self):
-        super(PlayerShoot, self).on_exit()
+        super().on_exit()
         PlayerShoot.ACTIVE_SHOOT = None
 
     def collide(self, other):
@@ -97,6 +104,7 @@ class PlayerShoot(Actor):
             # remove the alien and missile instances
             self.kill()
             other.kill()
+            kill_sfx.play()
 
     def update(self, delta_time):
         self.move(self.speed * delta_time)
@@ -104,13 +112,13 @@ class PlayerShoot(Actor):
 
 class Alien(Actor):
     def __init__(self, img, x, y, points, column=None):
-        super(Alien, self).__init__(img, x, y)
+        super().__init__(img, x, y)
 
         self.points = points
         self.column = column
 
     def on_exit(self):  # part of a superclass, called when removed from scene
-        super(Alien, self).on_exit()
+        super().on_exit()
         if self.column:
             self.column.remove(self)
 
@@ -125,7 +133,7 @@ class Alien(Actor):
 
 class AlienShoot(Actor):
     def __init__(self, x, y):
-        super(AlienShoot, self).__init__('img/shoot.png', x, y)
+        super().__init__('img/shoot.png', x, y)
 
         self.speed = Vector2(0, -400)
 
@@ -134,8 +142,10 @@ class AlienShoot(Actor):
 
 
 class AlienColumn:
-    def __init__(self, x: int, y: int):
-        alien_types = enumerate(['1', '1', '2', '2', '3'])
+    def __init__(self, x: int, y: int, swarm):
+        alien_types = enumerate(['3', '3', '2', '2', '1'])
+
+        self.swarm = swarm
 
         self.aliens = [
             Alien.from_type(x, y + i * 60, alien_type, self) for i, alien_type in alien_types
@@ -146,6 +156,7 @@ class AlienColumn:
 
     def danger_close(self, direction: int):
         if len(self.aliens) == 0:
+            self.swarm.remove(self)
             return False
 
         alien = self.aliens[0]
@@ -158,7 +169,7 @@ class AlienColumn:
             or x <= 50 and direction == -1
 
     def shoot(self):
-        if len(self.aliens) > 0 and random() < 0.001:  # 5% probability per second
+        if len(self.aliens) > 0 and random() < self.swarm.shoot_probability:  # 5% probability per second
             x, y = self.aliens[0].position
             return AlienShoot(x, y - 50)  # shoot 50px below origin
         else:
@@ -170,7 +181,7 @@ class Swarm:
         """
         :type game: GameLayer
         """
-        self.columns = [AlienColumn(x + i * 60, y) for i in range(10)]
+        self.columns = [AlienColumn(x + i * 60, y, self) for i in range(10)]
         self.gl = game
 
         self.shoot_probability = 0.0001
@@ -197,7 +208,14 @@ class Swarm:
     def side_reached(self):
         return any(map(lambda col: col.danger_close(self.direction), self.columns))
 
+    def remove(self, col):
+        self.columns.remove(col)
+
     def update(self, dt):
+        if len([0 for _ in self]) < 1:
+            self.gl.game_finish()
+            return
+
         self.elapsed += dt
         # loop because the ∆t may constitute more than one update in a single update() call
         # if it is longer than the period
@@ -240,14 +258,14 @@ class Swarm:
             period_mod = -0.07
             if self.period + period_mod > 0:
                 self.period += period_mod  # increase rate of alien movement (not distance)
-            self.shoot_probability += 0.002
+            self.shoot_probability += 0.0006
 
             self.speed = self.base_speed * self.level_modifier  # set actual speed
 
 
 class HUD(Layer):
     def __init__(self):
-        super(HUD, self).__init__()
+        super().__init__()
 
         w, h = director.get_window_size()
 
@@ -259,6 +277,8 @@ class HUD(Layer):
 
         self.level_text = Label("", font_size=18, color=(100, 80, 200, 255), bold=True, anchor_x="center")
         self.level_text.position = (w / 2, h - 40)
+
+        self.jumbo_text = None
 
         self.add(self.score_text)
         self.add(self.lives_text)
@@ -274,18 +294,25 @@ class HUD(Layer):
     def update_level(self, new_level: int):
         self.level_text.element.text = f"Lvl: {new_level}"
 
-    def show_over(self, message, color=(169, 18, 18, 255)):
+    def show_jumbo(self, message, color=(169, 18, 18, 255)):
         w, h = director.get_window_size()
 
         text = Label(message, color=color, font_size=50, anchor_x="center", anchor_y="center", bold=True)
         text.position = (w * 0.5, h * 0.5)
 
-        self.add(text)
+        self.jumbo_text = text
+        self.add(self.jumbo_text)
+
+    def hide_jumbo(self):
+        if self.jumbo_text:
+            self.remove(self.jumbo_text)
 
 
 class GameLayer(Layer):
-    def __init__(self, hud: HUD):
-        super(GameLayer, self).__init__()
+    def __init__(self, hud: HUD, music: Player):
+        super().__init__()
+
+        self.music_player = music
 
         w, h = director.get_window_size()
         self.width = w  # set layer to match window size
@@ -310,20 +337,31 @@ class GameLayer(Layer):
         self.schedule(self.game_loop)  # schedule game_loop() to be run every frame
 
     def create_player(self):
+        if self.player is not None:
+            self.player.kill()
+            self.player = None  # not having this caused a 'cannot find child' error before
         self.player = PlayerCannon(self.width * 0.5, 50)
         self.add(self.player)
 
         self.hud.update_lives(self.lives)
 
     def create_swarm(self, x, y):
+        if self.swarm is not None:
+            self.swarm = None
         self.swarm = Swarm(x, y, self)
         for alien in self.swarm:
             self.add(alien)
         self.hud.update_level(1)
 
-    def add_points(self, points=0):
-        self.score += points
+    def add_points(self, points=0, oust=False):
+        prev_m = self.score % 150
+        if oust:
+            self.score = points
+        else:
+            self.score += points
         self.hud.update_score(self.score)
+        if prev_m > self.score % 150:
+            self.swarm.increase_difficulty()
 
     def set_level(self, level=1):
         self.hud.update_level(level)
@@ -334,6 +372,7 @@ class GameLayer(Layer):
             self.player = None  # not having this caused a 'cannot find child' error before
             # it must mean that it was trying to kill the player
             # because it was 'not None' even when no player had been spawned
+            die_sfx.play()
         self.lives -= 1
         if self.lives < 1:
             self.hud.update_lives(0)
@@ -343,7 +382,17 @@ class GameLayer(Layer):
 
     def game_over(self):
         self.unschedule(self.game_loop)
-        self.hud.show_over('GAME OVER')
+        self.music_player.pause()
+        # self.music_player.delete()
+        self.hud.show_jumbo('GAME OVER')
+        self.schedule(listen_start)
+
+    def game_finish(self):
+        self.unschedule(self.game_loop)
+        self.music_player.pause()
+        # self.music_player.delete()
+        self.hud.show_jumbo('MISSION COMPLETE', (0, 200, 50, 255))
+        self.schedule(self.listen_next_mission)
 
     def collide_check(self, actor):
         """
@@ -360,7 +409,7 @@ class GameLayer(Layer):
     def game_loop(self, dt):
         # collision checking
         # for _, actor in self.children:
-
+        #
 
         # update actors and reset collision grid
         self.collman.clear()
@@ -384,19 +433,88 @@ class GameLayer(Layer):
         self.collide_check(PlayerShoot.ACTIVE_SHOOT)
         self.collide_check(self.player)
 
+    def listen_next_mission(self, _):
+        start_key_pressed = keyboard[key.ENTER] or keyboard[key.NUM_ENTER] or keyboard[key.SPACE]
+        if start_key_pressed:
+            self.unschedule(self.listen_next_mission)
+            self.create_swarm(100, 300)
+            # self.create_player()
+
+            self.lives = 3  # default hud values
+            self.hud.update_lives(self.lives)
+            # self.add_points(oust=True)  # keeping score because it isn't a new game
+            # self.hud.update_score(self.score)
+            self.hud.hide_jumbo()
+            self.music_player.play()
+
+            self.schedule(self.game_loop)
+
+
+class GameScene(Scene):
+    def __init__(self):
+        super().__init__()
+
+        self.hud_layer = HUD()
+        self.add(self.hud_layer, z=1)
+        self.game_layer = GameLayer(self.hud_layer, player)
+        self.add(self.game_layer, z=0)
+
+
+class TitleScreen(Layer):
+    def __init__(self):
+        super().__init__()
+
+        w, h = director.get_window_size()
+        self.width = w
+        self.height = h
+
+        self.title_text = Label("GAP OCCUPIERS", font_size=50, color=(100, 80, 200, 255), bold=True, anchor_x="center")
+        self.title_text.position = (w / 2, h - 130)
+
+        self.start_instruction_text = Label("Press ENTER to begin", font_size=18, color=(255, 180, 20, 255), bold=True,
+                                            anchor_x="center")
+        self.start_instruction_text.position = (w / 2, h / 2)
+
+        self.add(self.title_text)
+        self.add(self.start_instruction_text)
+
+
+class TitleScene(Scene):
+    def __init__(self):
+        super().__init__()
+
+        self.screen = TitleScreen()
+        self.add(self.screen)
+
+        self.schedule(listen_start)
+
+
+def listen_start(_):
+    start_key_pressed = keyboard[key.ENTER] or keyboard[key.NUM_ENTER] or keyboard[key.SPACE]
+    if start_key_pressed:
+        # self.end()
+        director.replace(GameScene())
+
 
 if __name__ == "__main__":
+    player = bg_music.play()
+    player.loop = True
     director.init(caption="Gap Occupiers", width=800, height=650)
 
     keyboard = key.KeyStateHandler()
     director.window.push_handlers(keyboard)
 
-    scene = Scene()
+    # scene = Scene()
+    # t_scene = TitleScene()
+    # g_scene = GameScene()
 
-    hud_layer = HUD()
-    scene.add(hud_layer, z=1)
+    # hud_layer = HUD()
+    # scene.add(hud_layer, z=1)
+    #
+    # game_layer = GameLayer(hud_layer, player)
+    # scene.add(game_layer, z=0)
 
-    game_layer = GameLayer(hud_layer)
-    scene.add(game_layer, z=0)
-
-    director.run(scene)
+    # director.run(scene)
+    # director.push(g_scene)
+    # director.push(t_scene)
+    director.run(TitleScene())
